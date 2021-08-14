6b7f5f32f5ccd52bff4fadaa44663c42bf7ebfb5
Katrin Hofstetter <katrin_hofstetter@gmx.de>
2021-08-14 09:55:31 +0200

    Director/Genre schema and model

--- models.js
+++ models.js
@@ -27,9 +27,26 @@ let userSchema = mongoose.Schema({
   favorites: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
 
+
+let directorSchema = mongoose.Schema({
+  name: {type: String, required: true},
+  bio: String,
+  birthyear: String,
+  movies: [String]
+});
+
+let genreSchema = mongoose.Schema({
+  name: {type: String, required: true},
+  description: String
+});
+
+
 let Movie = mongoose.model('Movie', movieSchema); // creation of models: you name it ('Movie' or 'User') first and then point to the created variable 
 let User = mongoose.model('User', userSchema);
+let Director = mongoose.model('Director', directorSchema);
+let Genre = mongoose.model('Genre', genreSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
-
+module.exports.Director = Director;
+module.exports.Genre = Genre;
