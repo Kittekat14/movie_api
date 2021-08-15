80dbe16a7e9a6d9b01cf21a24bdd161ae6f5b1b5
Katrin Hofstetter <katrin_hofstetter@gmx.de>
2021-08-15 15:50:45 +0200

    director and genre reference in each movie

--- models.js
+++ models.js
@@ -5,15 +5,8 @@ let movieSchema = mongoose.Schema({
   movieid: Number,
   title: {type: String, required: true},
   description: {type: String, required: true},
-  genre: {
-    name: String,
-    description: String
-  },
-  director: {
-    name: String,
-    bio: String,
-    birthyear: Date
-  },
+  genre: [ {type: mongoose.Schema.Types.ObjectId, ref: 'Genre'} ],
+  director: [ {type: mongoose.Schema.Types.ObjectId, ref: 'Director'} ],
   actors: [String],
   imageUrl: String,
   featured: Boolean
@@ -27,18 +20,16 @@ let userSchema = mongoose.Schema({
   favorites: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
 
-
-let directorSchema = mongoose.Schema({
-  name: {type: String, required: true},
-  bio: String,
-  birthyear: String,
-});
-
 let genreSchema = mongoose.Schema({
   name: {type: String, required: true},
   description: String
 });
 
+let directorSchema = mongoose.Schema({
+  name: {type: String, required: true},
+  bio: String,
+  birthyear: String
+});
 
 let Movie = mongoose.model('Movie', movieSchema); // creation of models: you name it ('Movie' or 'User') first and then point to the created variable 
 let User = mongoose.model('User', userSchema);
