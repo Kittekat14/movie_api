43767a4cd1d24167bd218a745ec670d75b0da301
Katrin Hofstetter <katrin_hofstetter@gmx.de>
2021-08-15 10:17:45 +0200

    deleting models for Director and Genre

--- models.js
+++ models.js
@@ -5,15 +5,8 @@ let movieSchema = mongoose.Schema({
   movieid: Number,
   title: {type: String, required: true},
   description: {type: String, required: true},
-  genre: {
-    name: String,
-    description: String
-  },
-  director: {
-    name: String,
-    bio: String,
-    birthyear: Date
-  },
+  genre: [ {type: mongoose.Schema.Types.ObjectId, ref: 'Genre'} ],
+  director: [ {type: mongoose.Schema.Types.ObjectId, ref: 'Director'} ],
   actors: [String],
   imageUrl: String,
   featured: Boolean
@@ -28,25 +21,8 @@ let userSchema = mongoose.Schema({
 });
 
 
-let directorSchema = mongoose.Schema({
-  name: {type: String, required: true},
-  bio: String,
-  birthyear: String,
-  movies: [String]
-});
-
-let genreSchema = mongoose.Schema({
-  name: {type: String, required: true},
-  description: String
-});
-
-
 let Movie = mongoose.model('Movie', movieSchema); // creation of models: you name it ('Movie' or 'User') first and then point to the created variable 
 let User = mongoose.model('User', userSchema);
-let Director = mongoose.model('Director', directorSchema);
-let Genre = mongoose.model('Genre', genreSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
-module.exports.Director = Director;
-module.exports.Genre = Genre;
